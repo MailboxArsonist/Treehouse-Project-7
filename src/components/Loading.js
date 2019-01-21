0ab8cc1c8874a5495ad698ebd0c057f65a500d5f
Matthew Dodd <matthew.dodd93@gmail.com>
2019-01-21 15:22:25 +0100

    Added loading and 404 components

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <>
+         <img className="spinner" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt="Loading"></img>
+         <h2 className="loading">Loading</h2>
+        </>
+    );
+}
+
+export default Loading;
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return(
+        <>
+         <h3>Whoops... 404!</h3>
+         <p>Sorry, the page you were looking for was not found</p>
+        </>
+    );
+}
+
+export default NotFound;
